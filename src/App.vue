5dfee0d213d309b81f8df6796e67cad35b65edec
Serhii Hudzenko <gudzsvmail@gmail.com>
2023-12-31 13:51:19 +0200

    header added

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14 h-screen">
+    <Header></Header>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-8 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+        <p class="text-slate-400">Shop the best sneakers</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-10">
+      <li>
+        <a class="flex items-center gap-3 text-gray-500 hover:text-black" href=""
+          ><img src="/cart.svg" alt="Cart" /><b>150 $</b></a
+        >
+      </li>
+      <li>
+        <a class="flex items-center gap-3 text-gray-500 hover:text-black" href=""
+          ><img src="/heart.svg" alt="Bookmarks" />Bookmarks</a
+        >
+      </li>
+      <li>
+        <a class="flex items-center gap-3 text-gray-500 hover:text-black" href=""
+          ><img src="/profile.svg" alt="Profile" />Profile</a
+        >
+      </li>
+    </ul>
+  </header>
+</template>
