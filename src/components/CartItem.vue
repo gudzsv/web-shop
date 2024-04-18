63490f21944d9fc5ab13f5c14193bf668968fead
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-04-18 13:25:26 +0300

    change src ro close image

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -21,7 +21,7 @@ defineProps({
         <img
           @click="() => emit('onClickRemove')"
           class="opacity-40 hover:opacity-100 cursor-pointer transition"
-          src="/web-shop/close.svg"
+          src="/close.svg"
           alt="close icon buton"
         />
       </div>
