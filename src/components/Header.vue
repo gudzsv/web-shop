8cba4dd009e48e046af3a3b65dc10909e17317b5
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-04-18 13:23:54 +0300

    remove sneakers dir from src

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,7 @@ defineProps({
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
     <router-link to="/">
       <div class="flex items-center gap-4">
-        <img src="/web-shop/logo.png" alt="logo" class="w-10" />
+        <img src="/logo.png" alt="logo" class="w-10" />
         <div>
           <h2 class="text-xl font-bold uppercase">Sneakers</h2>
           <p class="text-slate-400">Shop the best sneakers</p>
@@ -23,19 +23,19 @@ defineProps({
         @click="() => emit('openDrawer')"
         class="flex items-center gap-3 text-gray-500 hover:text-black"
       >
-        <img src="/web-shop/cart.svg" alt="Cart" />
+        <img src="/cart.svg" alt="Cart" />
         <b>{{ totalPrice }} $</b>
       </li>
 
       <li>
         <router-link class="flex items-center gap-3 text-gray-500 hover:text-black" to="/favorites">
-          <img src="/web-shop/heart.svg" alt="Bookmarks" /><span>Bookmarks</span>
+          <img src="/heart.svg" alt="Bookmarks" /><span>Bookmarks</span>
         </router-link>
       </li>
 
       <li>
         <a class="flex items-center gap-3 text-gray-500 hover:text-black" href=""
-          ><img src="/web-shop/profile.svg" alt="Profile" />Profile</a
+          ><img src="/profile.svg" alt="Profile" />Profile</a
         >
       </li>
     </ul>
